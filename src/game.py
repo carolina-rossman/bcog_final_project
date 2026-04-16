3e57caae32354046589e0c78f3a46f7277dbe68a
carolina-rossman <rossman5@illinois.edu>
2026-04-16 16:37:06 -0500

    working on game.py

--- src/game.py
+++ src/game.py
@@ -1,25 +1,41 @@
 import tkinter as tk 
+import random
 import FinalProject_PracticeCode
 
 class Display(): 
     def __init__(self, root): 
         pass 
         self.init_window()
+        self.load_dino
+        self.create_obstacles()
+        self.create_powerups()
+        self.create_powerdowns()
+
         # load scrolling background from other .py
+    def init_window(self): 
+        pass
     
     def load_dino(self): 
         self.dino = tk.PhotoImage(file = "../stimuli/dino.png")
 
     def create_powerups(self): 
-        pass
-    def create_powerdowns(self):
-        pass
+        self.jetpack = tk.PhotoImage(file = "../stimuli/_______")
+        self.immunity = tk.PhotoImage(file = "../stimuli/_____")
+        self.revival = tk.PhotoImage(file = "../stimuli/_____")
 
-    def init_window(self): 
-        pass
-    def avoid_obstacles(self): 
-        pass
-        # load obstacles 
+    def create_powerdowns(self):
+        self.speed_up = tk.PhotoImage(file = "../stimuli/_____")
+        
+    def create_obstacles(self): 
+        self.fence = tk.PhotoImage(file= "../stimuli/fence.png")
+        self.bush = tk.PhotoImage(file = "../stimuli/bush.png")
+        selected_obstacles = random.choice ([self.bush, self.fence])
+        # generate those images as background scrolls 
+        # use pygame
+
+
+        
+ 
 
 
 
