78b2c5777a9cc006bf7976714caac4dfb20bce5d
carolina-rossman <rossman5@illinois.edu>
2026-04-20 20:45:23 -0500

    start of changing start_screen.py

--- src/start_screen.py
+++ src/start_screen.py
@@ -1,72 +1,66 @@
-import tkinter as tk
+import pygame 
+import pygame.freetype
+from pygame.sprite import Sprite 
+from pygame.rect import Rect 
 import instructions_screen
+import sys
+
 class Display(): 
-    screen_size = (800, 600)
-    def __init__(self, root):
-        self.root = root
+    screen_size = pygame.Surface((800, 600))
+    def __init__(self):
+        pygame.init()
         self.screen_size_x = self.screen_size[0]
         self.screen_size_y = self.screen_size [1]
-        self.background_image = tk.PhotoImage(file = "../stimuli/start_screen_background.png")
+        self.canvas = pygame.display.set_mode(width = self.screen_size_x, height= self.screen_size_y)
+        self.background_image = pygame.image.load(file = "../stimuli/start_screen_background.png")
+        self.background_image = pygame.transform.scale(self.background_image, (self.screen_size_x, self.screen_size_y))
+        pygame.display.set_caption("Start Screen")
         self.init_window()
         self.create_interface_buttons()
 
     def init_window(self):
-       self.root.title("Start Screen")
-       self.canvas = tk.Canvas(self.root, width = self.screen_size_x, height= self.screen_size_y)
-       self.canvas.pack()
-       self.canvas.create_image(
-           self.screen_size_x // 2,
-           self.screen_size_y //2, 
-           image = self.background_image,
-       )
-       self.canvas.create_rectangle(
-           250, 
-           70, 
-           550, 
-           130, 
-           fill = "white",
-       )
-       self.canvas.create_text (
-           self.screen_size_x //2, 
-           100,
-           text = "Dino Dash",
-           font = ("Arial", 60, "bold"),
-           fill = "black",
-       )
+       self.canvas.blit(self.background_image)
+       pygame.draw.rect(self.canvas)
+       self.title_font = pygame.font.SysFont("Arial", 60, bold = True)
+       title = self.title.render ("Dino Dash")
+       self.canvas.blit (title)
+       pygame.display.update()
+
     def create_interface_buttons(self):
         # quit button 
-        quit_button = tk.Button(
-           self.root, 
-           text = "Quit",
-           font = ("Arial", 20),
-           fg = "black",
-           command = self.root.destroy,  
-           bd = 0,
-           highlightthickness=0,
-           relief= tk.SUNKEN,
-      )
-        quit_button.pack()
-        # go button
-        go_button = tk.Button(
-           self.root, 
-           text = "Start!",
-           font = ("Arial", 40, "bold"),
-           fg = "black",
-           command = self.go, 
-        )
-        go_button.pack()
-
-        self.canvas.create_window(400, 440, window= go_button)
-        self.canvas.create_window(400, 500, window=quit_button)
+        pygame.draw.rect(self.canvas, self.start_button)
+        pygame.draw.rect(self.canvas, self.quit_button)
+        self.button_font = pygame.font.SysFont("Arial", 20, bold=True)
+        start_text = self.button_font.render ("Start!", True)
+        quit_text = self.button_font.render ("Quit", True)
+        self.canvas.blit (start_text, (self.start_button.x, self.start_button.y))
+        self.canvas.blit(quit_text, (self.quit_button.x, self.quit_button.y))
+        pygame.display.update()
     def go(self):
-        self.root.destroy()
-        new_root = tk.Tk()
-        instructions_screen.Display(new_root)
-        new_root.mainloop()
+        running = True 
+        while running: 
+            self.init_window()
+            self.create_interface_buttons()
+            pygame.display.update()
+            for response in pygame.event.get(): 
+                if response.type == pygame.QUIT: 
+                    pygame.quit()
+                    sys.exit()
+            
+                if response.type == pygame.MOUSEBUTTONDOWN: 
+                    mouse_pos = response.pos
+                    if self.start_button.collidepoint(mouse_pos):
+                        instructions_screen.Display.go()
+                    if self.quit_button.collidepoint(mouse_pos): 
+                        pygame.quit()
+                        sys.exit()
+        instructions_screen.Display().run()
+        
+
 
 def main(): 
     my_display = Display()
-    my_display.root.mainloop()
+    my_display.go()
 
 if __name__ == "__main__":
     main()
