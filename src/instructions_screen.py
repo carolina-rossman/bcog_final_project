3e1eba4860579c71ffb444849a315d84fb6eb9b4
carolina-rossman <rossman5@illinois.edu>
2026-04-27 17:53:51 -0500

    changing instructions_screen to fit the start_screen.py

--- src/instructions_screen.py
+++ src/instructions_screen.py
@@ -1,63 +1,67 @@
-import tkinter as tk
+import pygame 
+import pygame.freetype
+import sys
 import base_dino
+
 class Display(): 
     screen_size = (800, 600)
-    def __init__(self, root):
-        self.root = root 
+    def __init__(self):
+        pygame.init()
         self.screen_size_x = self.screen_size[0]
         self.screen_size_y = self.screen_size [1]
-        self.background_image = tk.PhotoImage(file = "../stimuli/instructions_background.png")
+        self.canvas = pygame.display.set_mode(self.screen_size)
+        self.background_image = pygame.image.load("../stimuli/instructions_background.png")
+        self.background_image = pygame.transform.scale(self.background_image, (self.screen_size))
+        self.start_button = pygame.Rect(300, 250, 200, 50)
+        self.quit_button = pygame.Rect(300, 350, 200, 50)
+        pygame.display.set_caption("Instructions Screen")
         self.load_instructions()
-        self.init_window()
-        self.create_interface_buttons()
        
     def load_instructions(self): 
         with open ("../stimuli/instructions.txt", "r") as file: 
             self.instructions = file.read()
             
     def init_window(self):
-       self.root.title("Instructions Screen")
-       self.canvas = tk.Canvas(self.root, width = self.screen_size_x, height= self.screen_size_y)
-       self.canvas.pack()
-       self.canvas.create_image(
-           self.screen_size_x // 2,
-           self.screen_size_y //2, 
-           image = self.background_image,
-       )
-       self.canvas.create_text (
-           self.screen_size_x //2, 
-            100,
-            text = self.instructions,
-            font = ("Arial", 20, "bold"),
-            fill = "white",
-            width = 600,
-            anchor= "n",
-            justify = "left", 
-       )
+       self.canvas.blit(self.background_image, (0,0))
+       self.instructions_font = pygame.font.SysFont("Arial", 24)
+       instruction = self.instructions_font.render (self.instructions, True, (255, 255, 255))
+       instructions_rect = instruction.get_rect (center = (self.screen_size_x/2, 100))
+       self.canvas.blit (instruction, instructions_rect)
   
     def create_interface_buttons(self): 
-        go_button = tk.Button(
-           self.root, 
-           text = "Next",
-           font = ("Arial", 20, "bold"),
-           fg = "black",
-           command = self.go, 
-        )
-        go_button.pack()
-        self.canvas.create_window(400, 480, window= go_button)
+        pygame.draw.rect(self.canvas, (0, 200, 0), self.start_button)
+        pygame.draw.rect(self.canvas,(200, 0, 0), self.quit_button)
+        self.button_font = pygame.font.SysFont("Arial", 20, bold=True)
+        start_text = self.button_font.render ("Next!", True, (255, 255, 255))
+        quit_text = self.button_font.render ("Quit", True, (255, 255, 255))
+        self.canvas.blit (start_text, (self.start_button.x, self.start_button.y))
+        self.canvas.blit(quit_text, (self.quit_button.x, self.quit_button.y))
     
     def go(self):
-        self.root.destroy()
-        base_dino.run_game()
-        new_root = tk.Tk()
-        base_dino.Display(new_root)
-        new_root.mainloop()
+        clock = pygame.time.Clock()
+        running = True 
+        while running: 
+            self.init_window()
+            self.create_interface_buttons()
+            pygame.display.update()
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT: 
+                    pygame.quit()
+                    sys.exit()   
+                if event.type == pygame.MOUSEBUTTONDOWN: 
+                    print("Start clicked!")
+                    if self.start_button.collidepoint(event.pos):
+                        base_dino.Display.go()
+                    if self.quit_button.collidepoint(event.pos): 
+                        pygame.quit()
+                        sys.exit()
+            pygame.display.update
+            clock.tick(120)
 
 
 def main(): 
-    root = tk.Tk()
-    Display(root)
-    root.mainloop()
+    my_display = Display()
+    my_display.go()
 
 if __name__ == "__main__":
     main()
